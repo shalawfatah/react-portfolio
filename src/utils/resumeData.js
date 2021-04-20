9ef471fcf14d1c11a6999784c58ef63dec64e24a
Shalaw Fatah <shalaw.fatah@gmail.com>
2021-04-20 10:12:00 -0700

    added tech stack

--- src/utils/resumeData.js
+++ src/utils/resumeData.js
@@ -117,6 +117,7 @@ export default {
             tag: 'React',
             image: 'https://cdn.dribbble.com/users/4482322/screenshots/15076759/media/e7a3cf66762beb37318baba4f5bb3bab.png?compress=1&resize=1600x1200',
             title: 'Profile',
+            stack: ['GatsbyJS', 'Strapi CMS', 'Tailwind CSS'],
             description: 'This is a statically-generated React portfolio I built with GatsbyJS in the front-end and Strapi CMS in the backend. The tech used in this project includes major React features like hooks, modular design, API calls, etc.',
             caption: 'This is a statically-generated React portfolio I built with GatsbyJS in the front-end and Strapi CMS in the backend.',
             links: [
@@ -128,6 +129,7 @@ export default {
             tag: 'React',
             image: 'https://cdn.dribbble.com/users/4482322/screenshots/15425359/media/2bb9f97cbff231e0021fe75cb8733481.png?compress=1&resize=1600x1200',
             title: 'Worldwide Bee Website',
+            stack: ['GatsbyJS', 'LeafletJS', 'GIS', 'Strapi CMS', 'Tailwind CSS'],
             description: 'I built this website as a volunteer with a group of volunteers, from design to development, using GatsbyJS and Strapi CMS. Many technologies have been used in this website, including GIS [custom map and geolocation functionality], Tailwind CSS, MailChimp, and many others',
             caption: 'This is a website I built with React and Strapi, using many frontend tecnologies.',
             links: [
@@ -139,6 +141,7 @@ export default {
             tag: 'React',
             image: 'https://cdn.dribbble.com/users/4482322/screenshots/15076759/media/e7a3cf66762beb37318baba4f5bb3bab.png?compress=1&resize=1600x1200',
             title: 'Profile',
+            stack: ['GatsbyJS, Strapi CMS', 'Tailwind CSS'],
             description: 'This is a statically-generated React portfolio I built with GatsbyJS in the front-end and Strapi CMS in the backend. The tech used in this project includes major React features like hooks, modular design, API calls, etc.',
             caption: 'This is a statically-generated React portfolio I built with GatsbyJS in the front-end and Strapi CMS in the backend.',
             links: [
@@ -150,6 +153,7 @@ export default {
             tag: 'React',
             image: 'https://www.fossmint.com/wp-content/uploads/2020/03/Watch-Free-Movies-Online.png',
             title: 'Movie App',
+            stack: ['React', 'API', 'Styled Components'],
             description: 'This movie app is built with React and using MovieDB API.',
             caption: 'This movie app is built with React and using MovieDB API. Many features of React has been used here, including live search, hooks, and API.',
             links: [
@@ -161,6 +165,7 @@ export default {
             tag: 'React',
             image: 'https://image.shutterstock.com/image-vector/weather-forecast-poster-temperature-humidity-260nw-1711691812.jpg',
             title: 'Weather Progressive Web App',
+            stack: ['React', 'API', 'CSS'],
             description: 'Using React and Open Weather API, I created this app to check the weather. However, the main objective was to include the progressive web app (PW) functionality, which allows the app to work offline and be installed as a standalone app on smartphones and computers.',
             caption: 'This is a progressive web app created with React, can be installed on computers and smartphones',
             links: [
@@ -172,6 +177,7 @@ export default {
             tag: 'Wordpress',
             image: 'https://cdn.dribbble.com/users/4482322/screenshots/9910720/media/6d898ae77a749ee4637333bd2c99f0cc.jpg?compress=1&resize=1600x1200',
             title: 'Ideation Resources Limited',
+            stack: ['Wordpress', 'Elementor'],
             description: 'This website is built on Wordpress, using Elementor Page Builder and many other plugins. The client wanted a fast website with some illustrations to deliver the message that they want. ',
             caption: 'I built this website for a small business based in Vancouver',
             links: [
@@ -182,6 +188,7 @@ export default {
             tag: 'Wordpress',
             image: 'https://cdn.dribbble.com/users/4482322/screenshots/15195631/media/65604bfc1d485475b2a7883acb89d63e.png?compress=1&resize=1600x1200',
             title: 'Vancrown Home Service',
+            stack: ['Wordpress', 'Elementor', 'CSS'],
             description: 'This website is built on Wordpress, using Elementor Page Builder and several other plugins. SEO and speed was the focus for the client. I tried to make SEO especially in focus for this website.',
             caption: 'I built this portfolio for a Vancouver-based renovation company.',
             links: [
@@ -192,6 +199,7 @@ export default {
             tag: 'Wordpress',
             image: 'https://cdn.dribbble.com/users/4482322/screenshots/10594776/media/a962b277106fe2032d32f0fe92500e14.png?compress=1&resize=1600x1200',
             title: 'Personal Portfolio',
+            stack: ['Wordpress', 'Elementor', 'CSS'],
             description: 'This personal portfolio is made with Wordpress, using Elementor Page Builder, Astra theme, and with several modifications. Some plugins have been used to make the website better. ',
             caption: 'This is a personal portfolio made with Wordpress',
             links: [
@@ -202,6 +210,7 @@ export default {
             tag: 'Shopify',
             image: 'https://egnition.io/web/wp-content/uploads/2020/07/how-shopify-works.jpg',
             title: 'The Mane Avenue Website',
+            stack: ['Shopify', 'Debutify', 'CSS'],
             description: 'I built this website for a client in The Mane Avenue with Shopify. The client specifically asked for Shopify for the ease of use when it comes to ecommerce. The theme behind this is Debutify, many marginal apps have been used in the background to make the process smoother.',
             caption: 'I built this webshop for a client in The Mane Avenue',
             links: [
@@ -212,6 +221,7 @@ export default {
             tag: 'Shopify',
             image: 'https://cdn.dribbble.com/users/4482322/screenshots/9933736/media/1686bccfb8be9ab293e3aeb0f3824737.jpg?compress=1&resize=1600x1200',
             title: 'Fast Fashion Finds Website',
+            stack: ['Shopify', 'Debutify', 'CSS'],
             description: 'I built this website for a client in Fast Fashion Finds with Shopify. The client asked for an accessible webshop which she can run by herlsef without the need of a web developer to maintain. The theme behind this is Debutify, many marginal apps have been used in the background to make the process smoother.',
             caption: 'I built this webshop for a client in Fast Fashion Finds',
             links: [
@@ -222,6 +232,7 @@ export default {
             tag: 'Javascript',
             image: 'https://cdn.dribbble.com/users/4482322/screenshots/10115255/media/f2e5eeaa426c7a700bab2b002327c534.jpg?compress=1&resize=1600x1200',
             title: 'Web Developer Profile',
+            stack: ['Javascript', 'CSS', 'HTML'],
             description: 'I built this web developer profile with Vanilla Javascript, mostly using DOM manipulator to animate the website and make it work smoothly. Several animations have been used, including sliders, on mouse hover photo change, etc.',
             caption: 'A web developer profile built with Vanilla Javascript',
             links: [
